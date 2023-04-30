c7b2ebb42021dc555da279ec2a502aebff9d1a3a
yıldız <mesel4461@gmail.com>
2023-04-30 21:43:10 +0300

    as

--- src/App.js
+++ src/App.js
@@ -3,8 +3,10 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Merhaba nasılsın iyi misin?</h1>
+    <div className="App grid gap-4 ">
+      <h1 className="border border-solid py-[23px] hover:border-dotted sm:text-center bg-purple text-gray-light">
+        Merhaba nasılsın iyi misin?
+      </h1>
       <p>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt,
         sapiente. Non quod qui voluptatum suscipit, optio eaque culpa magni
@@ -14,7 +16,7 @@ function App() {
         src="https://www.planetware.com/wpimages/2020/02/france-in-pictures-beautiful-places-to-photograph-eiffel-tower.jpg"
         alt=""
       />
-      <ul>
+      <ul className="grid grid-cols-4 space-y-4 ">
         <a href="#">
           <li>ocak</li>
         </a>
